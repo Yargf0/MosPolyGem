1dcdaa3702cf44a676a3f29bb44fb3445c6f0bbb
Yar <Yar55rus@gmail.com>
2024-02-05 20:06:17 +0300

    BigFix+Connection

---
+++ Jem/Assets/Script/Event.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Event : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    [SerializeField] private List<GameObject> eventObjects;
+    [SerializeField] private List<GameObject> faileureObjects;
+    [SerializeField] private List<Transform> eventPosition;
+    [SerializeField] private List<GameObject> eventButton;
+    private int activeEventIndex=99;
+    [SerializeField] private int failTime;
+    public bool pressed=false;
+    public static Event Instance { get; private set; }
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+    private void Start()
+    {
+        NewEvent();
+    }
+    public void NewEvent()
+    {
+        pressed = false;
+        activeEventIndex = Random.Range(0, eventPosition.Count - 1);        
+        eventButton[activeEventIndex].SetActive(true);
+        eventObjects[activeEventIndex].SetActive(true);
+        Debug.Log(eventPosition[activeEventIndex].position);
+        StudentController.Instance.NewDestenation(eventPosition[activeEventIndex].position);
+        Debug.Log("New event " + activeEventIndex);
+    }
+    public void FailEvent()
+    {
+        Debug.Log("Event " + activeEventIndex + " faild");
+        player.SetActive(false);
+        faileureObjects[activeEventIndex].SetActive(true);
+        eventButton[activeEventIndex].SetActive(false);
+        StartCoroutine(whaitFail());
+    }
+    IEnumerator whaitFail()
+    {
+        yield return new WaitForSeconds(failTime);
+        StartCoroutine(whaitNewEvent());
+    }
+    public void CheckEvent()
+    {
+        if (!pressed)
+        {
+            FailEvent();
+        }
+        else
+        {
+            StartCoroutine(whaitNewEvent());
+        }        
+    }
+    IEnumerator whaitNewEvent()
+    {
+        Debug.Log("Whait for new event");
+        faileureObjects[activeEventIndex].SetActive(false);
+        eventObjects[activeEventIndex].SetActive(false);
+        eventButton[activeEventIndex].SetActive(false);
+        player.SetActive(true);
+        yield return new WaitForSeconds(5f);
+        NewEvent();
+    }
+    public int GetEvent()
+    {
+        return activeEventIndex;
+    }
+    public void PressButton()
+    {
+        pressed = true;
+        eventObjects[activeEventIndex].SetActive(false);
+        eventButton[activeEventIndex].SetActive(false);
+    }
+}
